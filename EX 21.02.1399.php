f19e5a899880b2ca77727cf1c2d606561919599a
SiNa <46754167+cnahx@users.noreply.github.com>
2020-05-12 13:12:10 +0430

    Update EX 21.02.1399.php

--- EX 21.02.1399.php
+++ EX 21.02.1399.php
@@ -1,13 +1,26 @@
-<!-- <?php
-echo ("<table height='10' width=10><tr>");
-for ($i=1; $i <= 10; $i++) {
-    echo ("<td>");
-    for ($a=1; $a <= 10; $a++) {
-        echo ("$i X $a = ");
-        echo $a * $i."<br>";
-    }
-    echo ("</td>");
+<?php
+
+$cols = 10;
+$rows = 10;
+
+echo "<table border=\"1\">";
+
+for ($r =0; $r < $rows; $r++){
+
+
+    echo('<tr>');
+
+    for ($c = 0; $c < $cols; $c++)
+        echo( '<td>' .$c*$r.'</td>');
+
+    echo('</tr>');
+
 }
-echo ("</tr></table>");
+
+echo("</table>");
+
+
+
+
+
 ?>
- -->
